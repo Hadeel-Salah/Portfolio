df859fce71a79d873f605fa2d267f47ece0f63c3
hadeel salah <hadeelsalah80@gmail.com>
2020-04-26 20:22:57 +0300

    Create About section

---
+++ client/src/Components/ChoicesComponents/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+class About extends Component {
+render() {
+return (
+    <div className="condiv">
+        <h1 className="s">About Me</h1>
+        <h4>About me Section</h4>
+        <p>
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+            Example Example Example Example Example Example
+ 
+
+
+        </p>
+
+        </div>
+)
+}
+}
+export default About
